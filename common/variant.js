ebedc6f7c1c5d90ac20a02c1f06e5635b3925411
vikopromotion <lehoannamkha2002@gmail.com>
2020-01-20 18:35:29 +0700

    Lmao relocate directory for all page for redirecting

--- common/variant.js
+++ common/variant.js
@@ -1,7 +1,7 @@
 var navbar =
-`<a class="unSelect" id="navbarAbout" href="./index.html">About</a>
-<a class="unSelect" id="navbarPost" href="./404.html">Posts</a>
-<a class="unSelect" id="navbarProduct"href="./products/products.html">Products</a>`;
+`<a class="unSelect" id="navbarAbout" href="../index.html">About</a>
+<a class="unSelect" id="navbarPost" href="../404.html">Posts</a>
+<a class="unSelect" id="navbarProduct"href="../products/products.html">Products</a>`;
 
 var credit =
 `Copyright © 2020 vikopromotion.<br/>Powered by Bootstrap 4.`;
