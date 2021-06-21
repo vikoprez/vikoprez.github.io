667e2accf1e5d127629f8e529dd47dd2f74ff7c4
vikopromotion <lehoannamkha2002@gmail.com>
2021-06-21 17:39:37 +0700

    For exam purpose

--- common/variant.js
+++ common/variant.js
@@ -4,7 +4,7 @@ var navbar =
 <a class="unSelect" id="navbarProduct"href="/products/products.html">Products</a>`;
 
 var credit =
-`Copyright © 2021 vikopromotion.<br/>Powered by Bootstrap 4.`;
+`Copyright © 2021 vikopromotion.<br/>Powered by <b>Bootstrap 4</b>.`;
 
 $(window).on("load", function(){
     $("#navbar").append(navbar);
